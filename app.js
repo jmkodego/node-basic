32447801725a7f32687fd44427f45a3c337a1fdb
JM Baldonado <john.baldonado@xurpas.com>
2023-06-13 21:23:15 +0800

    introduce pug and hbs as template engine in Express

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const path = require("path");
-const app = express();
 const bodyParser = require("body-parser");
+const { engine } = require("express-handlebars");
 
-const rootDir = require("./util/path");
+const app = express();
+
+app.engine(
+  "hbs",
+  engine({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -15,7 +22,7 @@ app.use(shopRoutes);
 app.use("/admin", adminData.routes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3001);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
-const path = require("path");
 const express = require("express");
-
 const router = express.Router();
-const rootDir = require("../util/path");
-
 const adminData = require("./admin");
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    products: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+  });
 });
 
 module.exports = router;
