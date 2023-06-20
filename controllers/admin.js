a215fbe5301a3e1a65b0fb4852dc300d0cbe0726
JM Baldonado <john.baldonado@xurpas.com>
2023-06-20 21:23:40 +0800

    introduce concept of query params and edit functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,34 @@
 const Product = require('../models/products');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false
   });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+
+  if (!editMode) {
+    return res.redirect("/")
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+
+  
 };
 
 exports.getProducts = (req, res, next) => {
@@ -24,7 +48,21 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedDesc = req.body.description;
+  const updatedImageUrl = req.body.imageUrl;
+
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+
+  updatedProduct.save();
+  res.redirect('/admin/products')
+  
+}
--- models/cart.js
+++ models/cart.js
@@ -21,7 +21,7 @@ module.exports = class Cart {
                cart = JSON.parse(fileContent);
             }
         // Analyze the cart => Find existing product
-            const existingProductIndex = cart.products.find(prod => prod.id === id);
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
             const existingProduct = cart.products[existingProductIndex];
             let updatedProduct;
 
@@ -30,7 +30,8 @@ module.exports = class Cart {
                 updatedProduct = { ...existingProduct };
                 updatedProduct.qty = updatedProduct.qty + 1;
                 cart.products = [...cart.products]
-                cart.products[existingProductIndex] = updatedProduct;
+                cart.products =
+                    cart.products[existingProductIndex] = updatedProduct;
             } else {
                 updatedProduct = { id: id, qty: 1 };
             cart.products = [...cart.products, updatedProduct]
