17d23980fee30e89403bd3cbfdc7c46f934af75e
JM Baldonado <john.baldonado@xurpas.com>
2023-07-14 20:20:20 +0800

    deploy to vercel

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,30 +15,32 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/")
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
-
-    if (!product) {
+  Product.findById(prodId).then(([product]) => {
+    const productData = product[0];
+    if (!productData) {
       return res.redirect("/");
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product
+      product: productData
     });
-  });
+  }).catch((err) => { console.log(err) });
 
-  
+
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        res.render('admin/products', {
-          prods: products,
-          pageTitle: 'Admin Products',
-          path: '/admin/products',
-        });
-      });
+  Product.fetchAll().then(([rows, fieldData]) => {
+    res.render('admin/products', {
+      prods: rows,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  }).catch((err) => {
+    console.log(err)
+  })
 }
 
 
@@ -48,9 +50,10 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save().then(() => {
+    res.redirect('/');
+  }).catch(err => { console.log(err) })
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -62,13 +65,15 @@ exports.postEditProduct = (req, res, next) => {
 
   const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
 
-  updatedProduct.save();
-  res.redirect('/admin/products')
-  
+  updatedProduct.save().then(() => {
+    res.redirect('/admin/products')
+  }).catch((err) => { console.log(err) });
+
 }
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products")
+  Product.deleteById(prodId).then(() => {
+    res.redirect("/admin/products")
+  }).catch((err) => { console.log(err) });
 }
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,40 +2,43 @@ const Product = require("../models/products")
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products',
     });
-  });
+  }).catch(err => {console.log(err)})
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {product: product, pageTitle: product.title, path: '/products'})
-  })
+  Product.findById(prodId).then(([product]) => {
+    res.render('shop/product-detail', { product: product[0], pageTitle: product.title, path: '/products' })
+  }).catch((err) => { console.log(err)})
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/',
     });
-  });
+  }).catch(err => { console.log(err) })
+
+
 }
 
 exports.getCart = (req, res, next) => {
   Cart.getCart(cart => {
-    Product.fetchAll(products => {
+
+    Product.fetchAll().then(([rows, fieldData]) => {
       const cartProducts = [];
-      for (const product of products) {
+      for (const product of rows) {
         const cartProductData = cart.products.find(prod => prod.id === product.id)
         if (cartProductData) {
-          cartProducts.push({productData: product, qty: cartProductData.qty })
+          cartProducts.push({ productData: product, qty: cartProductData.qty })
         }
       }
       res.render('shop/cart', {
@@ -43,25 +46,29 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
         products: cartProducts
       })
-    })
+    }).catch((err) => {console.log(err)})
+
   })
-  
+
 }
 
-exports.postCart = (req, res, next) => { 
+exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId,product.price)
-  })
+  Product.findById(prodId).then(([product]) => {
+    Cart.addProduct(prodId, product[0].price)
   res.redirect('/cart')
+
+  }).catch((err) => {
+    console.log(err)
+  })
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
+  Product.findById(prodId).then(([product]) => {
+    Cart.deleteProduct(prodId, product[0].price);
     res.redirect('/cart')
-  })
+  }).catch((err) => {console.log(err)})
 }
 
 exports.getOrders = (req, res, next) => {
