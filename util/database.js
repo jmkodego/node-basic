b5a94e6f53bf4ef7db13eed732249a09f6044da4
JM Baldonado <john.baldonado@xurpas.com>
2023-07-14 20:23:51 +0800

    deploy vercel

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node_complete',
-    password: ''
+    host: 'bcbfu5iotgoljv0v3x5r-mysql.services.clever-cloud.com',
+    user: 'uqvn4nwokyndqjit',
+    database: 'bcbfu5iotgoljv0v3x5r',
+    password: '0tEX743ME2OOwtjhitz'
 })
 
 module.exports = pool.promise();
